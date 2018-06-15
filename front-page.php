061e289ce1c984afec5d219c7d9ade3b862e2608
llister15 <llister@wonkasoft.com>
2018-06-15 00:04:55 -0700

    added featured image support

--- front-page.php
+++ front-page.php
@@ -18,6 +18,12 @@ $slide_image = ( !get_theme_mod( 'slide_image' ) ) ? get_template_directory_uri(
 			</div> <!-- .col -->
 		</div> <!-- .row -->
 </section><!-- #primary -->
+<?php 
+$featured_image_1 =  ( ! get_theme_mod( 'featured_image_1' ) ) ? '' : get_theme_mod( 'featured_image_1' );
+if ( $featured_image_1 == '' ) :
+
+else :
+?>
 <section class="container content-section">
 	<div class="row">
 		<div class="col text-center">
@@ -25,14 +31,37 @@ $slide_image = ( !get_theme_mod( 'slide_image' ) ) ? get_template_directory_uri(
 		</div> <!-- /col -->
 	</div> <!-- /row -->
 	<div class="row justify-content-center">
-		<div class="col text-center">
-			
+		<ul class="featured-images">
+	<?php 
+/**
+ * This is the end of the check for the first featured image
+ * then load section
+ */
+endif;
 
+		for ($i=1; $i < 4; $i++) :
+			$featured_image = ( ! get_theme_mod( 'featured_image_'.$i ) ) ? '' : get_theme_mod( 'featured_image_'.$i );
+		if ( $featured_image != '' ) :
+	?>
 			
-		</div> <!-- /col -->
+			<li class="featured-item"><img src="<?php echo $featured_image; ?>" /></li>
+<?php
+
+/**
+ * This is the end of the check for featured images
+ */
+endif;
+
+ /**
+ * This is the end of the for loop that loads all featured images
+ */
+endfor; ?>
+		</ul> <!-- /ul -->
 	</div> <!-- /row -->
 </section> <!-- .container-fluid contemt-section -->
-<?php 
+
+<?php
+
 $ig_code =  ( ! get_theme_mod( 'ig_code' ) ) ? '' : get_theme_mod( 'ig_code' );
 if ( $ig_code == '' ) :
 
