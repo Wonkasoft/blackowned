d4e1fcc9aba6b927d8512a250ce7e187126ee464
MrLister <rlister@wonkasoft.com>
2018-06-19 02:13:12 -0700

    almost done with sliders

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -4,7 +4,10 @@
 	if ( document.querySelector( '.home') ) {
     if ( document.querySelectorAll( '.wonka-slider-images') ) { 
     	var wonka_sliders = document.querySelectorAll( '.wonka-slider-images'),
-    	wonka_slider_controls = document.querySelectorAll( '.control-btn' );
+    	wonka_slider_controls = document.querySelectorAll( '.control-btn' ),
+    	wonka_slider_indicators = document.querySelectorAll( '.slider-indicators li' );
+
+    	wonka_slider_indicators.forEach( control_listener );
     	wonka_sliders.forEach( function( element ) { wonka_slider_setup( element ); } );
     	wonka_slider_controls.forEach( control_listener );
     }
@@ -37,12 +40,14 @@ function slide_setup(el) {
 		slide_control = el.target.parentElement;
 	}
 
-	if ( el.target.nodeName == 'LI' ) {
-		slide_control = el.target;
-	}
-	
 	direction = slide_control.getAttribute('data-slider-btn');
 	current_parent = slide_control.parentElement;
+
+	if ( slide_control.nodeName == 'LI' ) {
+		current_parent = slide_control.parentElement.parentElement;
+		direction = 'next';
+	}
+	
 	current_dot = current_parent.querySelector( '.indicator-dot.active' );
 	current_slide = current_parent.querySelector( '.wonka-slider-item.active' );
 
@@ -77,6 +82,49 @@ function do_slide( slider_obj ) {
 	if ( slider_obj.direction === 'previous' ) {
 		slider_obj.cur_indicator.classList.remove( 'active' );
 		slider_obj.cur_slide.classList.add( 'slide-right' );
-		slider_obj.next_slide.classList.add( 'previous' );
+		setTimeout( function() {
+			slider_obj.cur_slide.style.left = '110%';
+			setTimeout( function() {
+				slider_obj.cur_slide.style.display = 'none';
+				slider_obj.cur_slide.classList.remove( 'active');
+				slider_obj.cur_slide.classList.remove( 'slide-right');
+				slider_obj.cur_slide.removeAttribute( 'style' );
+				slider_obj.next_slide.classList.add( 'previous' );
+				slider_obj.next_slide.classList.add( 'active' );
+				setTimeout( function() {
+					slider_obj.next_slide.style.right = '0';
+					slider_obj.next_indicator.classList.add( 'active' );
+					setTimeout( function() {
+						slider_obj.next_slide.removeAttribute( 'style' );
+						slider_obj.next_slide.classList.remove( 'previous' );
+					}, 500);
+				}, 500);
+			}, 500);
+		}, 300);
 	}
+
+	if ( slider_obj.direction === 'next' ) {
+		slider_obj.cur_indicator.classList.remove( 'active' );
+		slider_obj.cur_slide.classList.add( 'slide-left' );
+		setTimeout( function() {
+			slider_obj.cur_slide.style.right = '110%';
+			setTimeout( function() {
+				slider_obj.cur_slide.style.display = 'none';
+				slider_obj.cur_slide.classList.remove( 'active');
+				slider_obj.cur_slide.classList.remove( 'slide-left');
+				slider_obj.cur_slide.removeAttribute( 'style' );
+				slider_obj.next_slide.classList.add( 'next' );
+				slider_obj.next_slide.classList.add( 'active' );
+				setTimeout( function() {
+					slider_obj.next_slide.style.left = '0';
+					slider_obj.next_indicator.classList.add( 'active' );
+					setTimeout( function() {
+						slider_obj.next_slide.removeAttribute( 'style' );
+						slider_obj.next_slide.classList.remove( 'next' );
+					}, 500);
+				}, 500);
+			}, 500);
+		}, 300);
+	}
+
 } 
