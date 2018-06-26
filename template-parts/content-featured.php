7cae39a9beeb1068938de47369f54b909b48ff04
MrLister <rlister@wonkasoft.com>
2018-06-25 23:24:17 -0700

    fix modules

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -10,30 +10,38 @@
 $i = 1;
 global $woo_cats;
 foreach ( $woo_cats as $cat ) :
+	var_dump($cat);
+	$cat_id = $cat->cat_ID;
 	$thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); 
 	$image = wp_get_attachment_url( $thumbnail_id );
 	if ( $i == 1 ) : ?>
 		<div class="row featured-modules-row">
 			<div class="featured-module">
+			<a href="<?php echo get_category_link( $cat_id ); ?>" class="featured-module-link">
 				<div class="img-container">
 					<img src="<?php echo $image ?>" />
 				</div> <!-- /img-container -->
 				<h5><?php echo $cat->name; ?></h5>
+			</a> <!-- /featured-module-link -->
 			</div> <!-- /featured-module -->
 	<?php elseif ( $i == count( $woo_cats ) ) : ?>
 			<div class="featured-module">
+				<a href="<?php echo get_category_link( $cat_id ); ?>" class="featured-module-link">
 				<div class="img-container">
 					<img src="<?php echo $image ?>" />
 				</div> <!-- /img-container -->
 				<h5><?php echo $cat->name; ?></h5>
+				</a> <!-- /featured-module-link -->
 			</div> <!-- /featured-module -->
 		</div> <!-- /row -->
 	<?php else : ?>
 		<div class="featured-module">
+			<a href="<?php echo get_category_link( $cat_id ); ?>" class="featured-module-link">
 			<div class="img-container">
 				<img src="<?php echo $image ?>" />
 			</div> <!-- /img-container -->
 			<h5><?php echo $cat->name; ?></h5>
+			</a> <!-- /featured-module-link -->
 		</div> <!-- /featured-module -->
 	<?php endif;
 	$i++;
