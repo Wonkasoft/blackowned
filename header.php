2d6b024ed6e7df8cc566fc3ca51cbe22ef0f1e48
MrLister <m_r_lister@yahoo.com>
2018-07-04 13:51:35 -0700

    fixing package pages

--- header.php
+++ header.php
@@ -38,13 +38,13 @@
 								?>
 								<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 								<?php
-							endif;
+								$blackowned_description = get_bloginfo( 'description', 'display' );
+								if ( $blackowned_description || is_customize_preview() ) :
+									?>
+								<p class="site-description"><?php echo $blackowned_description; /* WPCS: xss ok. */ ?></p>
+							<?php endif; 
+							endif; ?>
 
-							$blackowned_description = get_bloginfo( 'description', 'display' );
-							if ( $blackowned_description || is_customize_preview() ) :
-								?>
-							<p class="site-description"><?php echo $blackowned_description; /* WPCS: xss ok. */ ?></p>
-						<?php endif; ?>
 						</div> <!-- .custom-logo -->
 					</div> <!-- .col-2 -->
 					<div class="col-md-7">
